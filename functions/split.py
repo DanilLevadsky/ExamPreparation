1cedfa4de631413a2ad555f7b1b244138c77dc2a
Danil Levadsky <danillevadsky@gmail.com>
2020-01-04 23:29:11 +0200

    Update split.py

--- functions/split.py
+++ functions/split.py
@@ -1,13 +1,32 @@
 def split(string, pattern=' '):
 	''' analog of split method'''
 	array = []
-	for s in string:
-		if s == pattern:
-			array.append(string[:string.index(s)])
-			string = string[(string.index(s) + 1):]
-			if string.count(pattern) == 0  and len(string) > 0:
-				array.append(string)	
+	if pattern in string:
+		if len(pattern) == 1:	
+			for s in string:
+				if s == pattern:
+					array.append(string[:string.index(s)])
+					string = string[(string.index(s) + 1):]
+					if string.count(pattern) == 0  and len(string) > 0:
+						array.append(string)	
+			for elem in array:
+				if elem == '':
+					array.remove(elem)
+		elif len(pattern) > 1:
+			if string[:len(pattern)-1] == pattern:
+				string = string[len(pattern):]
+			elif string[-len(pattern):] == pattern:
+				string = string[:-len(pattern)]
+			while pattern in string:	
+				index = string.find(pattern)
+				array.append(string[:index])
+				if pattern in string:
+					string = string[index + len(pattern):]
+				if pattern not in string:
+					array.append(string)	
+				index = None
+	else: raise Exception('Pattern did`t found')
 	for elem in array:
 		if elem == '':
-			array.remove(elem)			
+			array.remove(elem)							
 	return array
