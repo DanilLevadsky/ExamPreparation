9ca33a61cb39847f8caea90081243178dc7135d3
Danil Levadsky <danillevadsky@gmail.com>
2020-01-04 23:07:07 +0200

    Update split.py

--- functions/split.py
+++ functions/split.py
@@ -5,6 +5,9 @@ def split(string, pattern=' '):
 		if s == pattern:
 			array.append(string[:string.index(s)])
 			string = string[(string.index(s) + 1):]
-			if string.count(pattern) == 0:
+			if string.count(pattern) == 0  and len(string) > 0:
 				array.append(string)	
+	for elem in array:
+		if elem == '':
+			array.remove(elem)			
 	return array
